f9c516ef1d61e4bb4a079675a1c12f4cdb127d08
joetechem <joe@techemstudios.com>
2017-10-05 11:32:08 -0400

    notes after tiny tech

---
+++ tiny_tech/notes.py
@@ -0,0 +1,10 @@
+# 10/2
+    # intros
+    # what is technology? computers? program?
+    # discussed recipes and steps to make pb&j
+    # discussed sequences. What if the recipe was out of order?
+    # out of seat programming activity
+    # Kodable
+        # created kodable class instance for tracking progress
+            # starting next week
+    # very young group
