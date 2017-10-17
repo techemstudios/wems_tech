dcdd2d6b5f4a8d7be95d473dd265c89889f9db9b
joetechem <joe@techemstudios.com>
2017-10-17 13:22:47 -0400

    update to notes and materials

--- tiny_tech/notes.py
+++ tiny_tech/notes.py
@@ -8,3 +8,18 @@
         # created kodable class instance for tracking progress
             # starting next week
     # very young group
+
+# 10/9
+    # Columbus Day: no school
+
+# 10/16
+    # review what is technology? Computer? Program? Sequence?
+    # Make an algorithm for your morning routine
+        # The steps for a typical morning routine were provided on slips of construction paper
+        # students place/glue the steps in order (for the most part) on a seperate sheet of paper
+    # Kodable
+        # first saved profile/progress for kodable app
+        # finished sequences
+        # discussed conditions: "if this happens, then this happens"
+        # example: "if you eat everything on your dinner plate, then you can..."
+        # started first level of conditions
