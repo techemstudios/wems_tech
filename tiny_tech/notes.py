80526e0becf6eccd075561a80f9b007fbf31ca33
joetechem <joe@techemstudios.com>
2017-11-06 14:55:30 -0500

    generalized record sheet/updated notes

--- tiny_tech/notes.py
+++ tiny_tech/notes.py
@@ -23,3 +23,26 @@
         # discussed conditions: "if this happens, then this happens"
         # example: "if you eat everything on your dinner plate, then you can..."
         # started first level of conditions
+
+# 10/23
+    # Layers of a Computer System
+        # Hands-on activity, drawing and putting the layers together to take home
+    # ended with Kodable
+
+# 10/30
+    # Kodable and Lightbot
+
+# 11/6
+    # From Tiny Tech lesson plan, discussed conditions
+    # used deck of cards for simple conditional game
+        # Played several differnet way, or created several different programs using
+        # conditonal rules, kept score for some
+    # Kodable: refocus on what conditions actually are and where we can find them/how to recognize them (in real life too),
+    # and how to approach solving them in Kodable.
+
+    # Tried our hand out creating our own puzzles in Kodable
+        # However, group may be too young.. will try another step-by-step approach next week.
+
+
+
+
